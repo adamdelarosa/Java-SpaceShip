c4a616845f85109e058028efe19de0bdb0039aed
ROSA <gojiradam@gmail.com>
2016-05-05 11:19:40 +0300

    SpaceShip

--- src/sample/SpaceShip.java
+++ src/sample/SpaceShip.java
@@ -65,8 +65,6 @@ public class SpaceShip  {
         shipAnimation = new Thread(()->{
             while (true)
 
-
-
                 try {
                     nodeSpaceShip.setImage(heroImage1);
                     shipAnimation.sleep(300);
