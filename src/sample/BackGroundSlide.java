3305e9f7767d55dffbe5f29ab172167b1c591cf9
ROSA <gojiradam@gmail.com>
2016-06-06 12:11:31 +0300

    SpaceShip

--- src/sample/BackGroundSlide.java
+++ src/sample/BackGroundSlide.java
@@ -44,9 +44,6 @@ public class BackGroundSlide {
                     node.setTranslateY(Math.sin(-400) * d + height);
                 }
             }
-
-
-
         }.start();
         HBox backgroundBox = new HBox();
         backgroundBox.getChildren().addAll(nodes);
